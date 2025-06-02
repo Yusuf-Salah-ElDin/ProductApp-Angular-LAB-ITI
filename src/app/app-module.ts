867ccdc5c2c00cfd58c995c48659ccad243c4782
Yusuf-Salah-ElDin <yusuf_mustafa1553@outlook.com>
2025-06-02 16:15:25 +0300

    uploaded

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,16 +1,26 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductsComponent } from './components/products/products.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    App
+    App,
+    NavbarComponent,
+    ProductsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  standalone: false,
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent {
+  
+
+  products = [
+    {id: 1, title: 'Arabian Sword', stock: 25, category: 'Swords', price: 2000, image: 'arabian-sword.jpg'},
+    {id: 1, title: 'Indian Sword', stock: 25, category: 'Swords', price: 1500, image: 'indian-sword.jpg'},
+    {id: 1, title: 'Arabian Armour', stock: 25, category: 'Armours', price: 6000, image: 'arabian-armour.jpg'},
+    {id: 1, title: 'Crusade Armour', stock: 25, category: 'Armours', price: 5800, image: 'crusade-armour.jpg'},
+    {id: 1, title: 'Arabian Horse', stock: 25, category: 'Horses', price: 9000, image: 'arabian-horse.jpg'},
+    {id: 1, title: 'Crusade Horse', stock: 25, category: 'Horses', price: 8500, image: 'crusade-horse.jpg'}
+  ];
+
+  categories: string[] = ['Swords', 'Armours', 'Horses'];
+
+  filterValues = {
+    category: '',
+    title: '',
+    maxPrice: null as number | null
+  };
+
+  displayValues = {
+    category: '',
+    title: '',
+    maxPrice: null as number | null
+  };
+
+  updateFilterDisplay(): void {
+    this.displayValues = { ...this.filterValues };
+  }
+
+  getStockClass(stock: number): string {
+    if (stock < 20) return 'text-danger';
+    if (stock < 40) return 'text-warning';
+    return 'text-success';
+  }
+
+
+}
